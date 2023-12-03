ab3763bcf2a24161ba672b013a81bce65aa56a49
Vera <vera@macbook-pro.mynetworksettings.com>
2023-12-03 17:19:03 -0500

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ImageInput from './components/ImageInput'
+
+import { useState, ChangeEvent } from 'react';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState<string | null>(null);;
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      className="App"
+      sx={{
+        display: 'flex',
+        width: '100%',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+      <Box sx={{ display: 'flex', height: '500px', width: '90%' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '50%' }}>
+          {image && <img src={image} alt="Uploaded" style={{
+            objectFit: 'cover', maxWidth: '100%', maxHeight: '100%'
+          }} />}
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '50%' }}>
+          <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+            Upload file
+            <ImageInput type="file" onChange={handleFileChange} />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
