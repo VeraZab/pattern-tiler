652cf4eedca34699902ceb5ec8274917a52ed062
Vera <vera@macbook-pro.mynetworksettings.com>
2023-12-08 12:07:11 -0500

    some header adjustments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,32 @@
 import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
+import HelpIcon from '@mui/icons-material/Help';
 
 const Header = () => {
+    const aboutTilemaker = 'This is a simple patternmaking tool that allows you to use a tileable image ' +
+        'and repeat it any number of times to create a pattern of desired scale and dimensions. ' +
+        'Ready for printing!';
+
     return (
         <Box sx={{
             display: 'flex',
             justifyContent: 'space-between',
             alignItems: 'center',
-            width: '100vw',
             height: '45px',
             padding: '20px'
         }}>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <img src="pattern-tiler-logo.png" style={{ width: '80px' }} />
-                <Box sx={{ fontSize: '30px', fontWeight: 'bold', fontFamily: 'monospace' }}>Pattern Tiler</Box>
+                <img src="pattern-tiler-logo.png" style={{ width: '50px', paddingRight: '15px' }} />
+                <Box sx={{ fontSize: '30px', fontWeight: 'bold', fontFamily: 'monospace', }}>Pattern Tiler</Box>
             </Box>
-            <Box>About</Box>
+            <Box>
+                <Tooltip title={aboutTilemaker}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Box sx={{ paddingRight: '15px' }}>About this tool</Box>
+                        <HelpIcon />
+                    </Box>
+                </Tooltip>
+            </Box>
         </Box>
     );
 }
