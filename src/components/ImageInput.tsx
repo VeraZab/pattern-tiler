d6ab540f9a9447fc91a2b0f4ed6d0577a598aea8
Vera <vera.zabeida@gmail.com>
2024-01-09 14:21:00 -0500

    type css properties

--- src/components/ImageInput.tsx
+++ src/components/ImageInput.tsx
@@ -1,21 +1,23 @@
+import { CSSProperties } from "react";
+
 import { ImageInputProps } from "../types/imageInput";
+
+const imageInputStyles: CSSProperties = {
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+}
 
 const ImageInput: React.FC<ImageInputProps> = ({ onChange }) => (
   <input
     type="file"
-    style={
-      {
-        clip: "rect(0 0 0 0)",
-        clipPath: "inset(50%)",
-        height: 1,
-        overflow: "hidden",
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        whiteSpace: "nowrap",
-        width: 1,
-      }
-    }
+    style={imageInputStyles}
     onChange={onChange} />
 )
 
